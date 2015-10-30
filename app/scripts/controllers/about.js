fc7e9c02a993c8c46388ab7475c80b68c8542e92
michaelatang <michaelandrewtang@gmail.com>
2015-10-30 15:42:11 -0400

    first commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name fleetHoundSpApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the fleetHoundSpApp
+ */
+angular.module('fleetHoundSpApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name fleetHoundSpApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the fleetHoundSpApp
+ */
+angular.module('fleetHoundSpApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
